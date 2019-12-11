104e1f9a79441360d5dea57bcaf7d359b3fc23ed
Volt <sonianadashkevich@gmail.com>
2019-12-11 17:25:06 +0100

    zadanie domowe 1, nie dziala

---
+++ src/main/java/org/javastart/zajecia1012/Zajecia1012Application.java
@@ -0,0 +1,19 @@
+package org.javastart.zajecia1012;
+
+import org.javastart.zajecia1012.controller.spring.TestConnector;
+import org.javastart.zajecia1012.controller.spring.zadanie2.TestControllerZ2;
+import org.javastart.zajecia1012.controller.spring.zadanie2.UserRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Zajecia1012Application {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Zajecia1012Application.class, args);
+        //TestConnector bean = context.getBean(TestConnector.class);
+        //bean.saveToDatabase();
+    }
+
+}
